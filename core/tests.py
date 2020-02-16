b777223df2371471af221f7dbd28d1eac8613cfc
Alexander Khoroshavin <neutok@gmail.com>
2020-02-16 22:55:25 +0300

    Update tests

--- core/tests.py
+++ core/tests.py
@@ -1,10 +1,10 @@
 from django.shortcuts import resolve_url
-from django.test import Client, TransactionTestCase
+from django.test import Client, TestCase
 
-from core.models import Menu, MenuPoint
+from core.models import *
 
 
-class TestMenu(TransactionTestCase):
+class TestMenu(TestCase):
     def setUp(self):
         """
         Create menu 'main' with the structure:
@@ -17,21 +17,25 @@ class TestMenu(TransactionTestCase):
             'newspapers'
 
         """
-        menu = Menu.objects.create(title='main')
-        about = MenuPoint.objects.create(menu=menu, title='about', url_name='core:about')
-        about_us = MenuPoint.objects.create(menu=menu, title='about us', url_name='core:about_us', parent=about)
-        self.about_you = \
-            MenuPoint.objects.create(menu=menu, title='about you', url_name='core:about_you', parent=about_us)
-        MenuPoint.objects.create(menu=menu, title='about them', url_name='core:about_them', parent=about_us)
+        self.menu = Menu.objects.create(title='main')
+        self.about = MenuPoint.objects.create(menu=self.menu, title='about', url_name='core:about')
+        self.about_us = MenuPoint.objects.create(menu=self.menu, title='about us',
+                                                 url_name='core:about_us', parent=self.about)
+        self.about_you = MenuPoint.objects.create(menu=self.menu, title='about you',
+                                                  url_name='core:about_you', parent=self.about_us)
+        self.about_them = MenuPoint.objects.create(menu=self.menu, title='about them',
+                                                   url_name='core:about_them', parent=self.about_us)
+        self.news = MenuPoint.objects.create(menu=self.menu, title='news', url_name='core:news')
+        self.newspapers = MenuPoint.objects.create(menu=self.menu, title='newspapers',
+                                                   url_name='core:newspapers', parent=self.news)
 
-        news = MenuPoint.objects.create(menu=menu, title='news', url_name='core:news')
-        MenuPoint.objects.create(menu=menu, title='newspapers', url_name='core:newspapers', parent=news)
+        self.client = Client()
 
     def test_number_queries_to_database_is_one(self):
         # No active menu point.
         with self.assertNumQueries(1):
-            Client().get(resolve_url('core:index'))
+            self.client.get(resolve_url('core:index'))
 
         # Menu point 'about you' is active.
         with self.assertNumQueries(1):
-            Client().get(resolve_url(self.about_you.url_name))
+            self.client.get(resolve_url(self.about_you.url_name))

eb72fa02f0d1bbeb6cc3ee6648b0c2e8a9b34d24
Alexander Khoroshavin <neutok@gmail.com>
2020-02-17 15:08:04 +0300

    Update admin.py
    Add Menu and MenuPoint

--- core/admin.py
+++ core/admin.py
@@ -1,1 +1,19 @@
 from django.contrib import admin
+
+from core.models import *
+
+
+class MenuPointInline(admin.TabularInline):
+    model = MenuPoint
+
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    fields = ['title']
+    inlines = [MenuPointInline]
+
+
+@admin.register(MenuPoint)
+class MenuPointAdmin(admin.ModelAdmin):
+    list_display = ['title', 'menu', 'depth', 'parent', 'url_name']
+    ordering = ['depth']
